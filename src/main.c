2d651194791737061cab17712566b0bf53111bba
Julian Kylander <julian.kylander@gmail.com>
2024-11-13 01:32:45 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,9 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool hadError;
-
-enum TokenList {
+typedef enum {
     LEFT_PAREN, RIGHT_PAREN,
     LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS,
@@ -13,6 +11,7 @@ enum TokenList {
 
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
 
     IDENTIFIER, STRING, NUMBER,
@@ -23,21 +22,23 @@ enum TokenList {
     EOF_TOKEN,
 
     TOKEN_COUNT,
-};
+} TokenList;
 
 typedef struct {
     char *contents;
     size_t length;
-    size_t lines;
+    size_t line;
 } FileData;
 
 typedef struct {
-    int type;
+    TokenList type;
     char *lexeme;
+    int line;
+    bool error;
 } Token;
 
 struct method_string_pair {
-    enum TokenList type;
+    TokenList type;
     char *str;
 } known_methods[] = {
     {LEFT_PAREN, "LEFT_PAREN"}, {RIGHT_PAREN, "RIGHT_PAREN"},
@@ -47,6 +48,7 @@ struct method_string_pair {
 
     {BANG, "BANG"}, {BANG_EQUAL, "BANG_EQUAL"},
     {EQUAL, "EQUAL"}, {EQUAL_EQUAL, "EQUAL_EQUAL"},
+    {GREATER, "GREATER"}, {GREATER_EQUAL, "GREATER_EQUAL"},
     {LESS, "LESS"}, {LESS_EQUAL, "LESS_EQUAL"},
 
     {IDENTIFIER, "IDENTIFIER"}, {STRING, "STRING"}, {NUMBER, "NUMBER"},
@@ -58,107 +60,17 @@ struct method_string_pair {
     {EOF_TOKEN, "EOF"},
 };
 
-enum TokenList type_from_str(char *str) {
-    int n = sizeof(known_methods) / sizeof(known_methods[0]);
-    for (int i = 0; i < n; ++i) {
-        if (strcmp(str, known_methods[i].str) == 0) {
-            return known_methods[i].type;
-        }
-    }
-    hadError = true;
-    return -1;
-}
-
-char *str_from_type(enum TokenList type) {
-    return known_methods[type].str;
-}
+TokenList type_from_str(char *str);
+char *str_from_type(TokenList type);
+FileData *read_file_contents(const char *filename);
+Token *scan_token(FileData *data, int *position);
+void print_token(Token token);
+bool match(FileData *data, int *position, char c);
+bool isAtEnd(int position, int len);
+char advance(FileData *data, int *position);
+void interpreter(FileData *data);
 
 FileData *read_file_contents(const char *filename);
-
-Token *scan_tokens(FileData *data, size_t *num_tokens) {
-    Token *tokens = malloc((data->length + 1) * sizeof(Token));
-    data->lines++;
-
-    for (int i = 0; i < data->length; i++) {
-        Token *token = &tokens[*num_tokens];
-        (*num_tokens)++;
-
-        switch (data->contents[i]) {
-            case '(': {
-                token->type = LEFT_PAREN;
-                token->lexeme = "(";
-            } break;
-
-            case ')': {
-                token->type = RIGHT_PAREN;
-                token->lexeme = ")";
-            } break;
-
-            case '{': {
-                token->type = LEFT_BRACE;
-                token->lexeme = "{";
-            } break;
-
-            case '}': {
-                token->type = RIGHT_BRACE;
-                token->lexeme = "}";
-            } break;
-
-            case ',': {
-                token->type = COMMA;
-                token->lexeme = ",";
-            } break;
-
-            case '.': {
-                token->type = DOT;
-                token->lexeme = ".";
-            } break;
-
-            case '-': {
-                token->type = MINUS;
-                token->lexeme = "-";
-            } break;
-
-            case '+': {
-                token->type = PLUS;
-                token->lexeme = "+";
-            } break;
-
-            case ';': {
-                token->type = SEMICOLON;
-                token->lexeme = ";";
-            } break;
-
-            case '*': {
-                token->type = STAR;
-                token->lexeme = "*";
-            } break;
-
-            default: {
-                fprintf(stderr, "[line %zu] Error: Unexpected character: %c\n", data->lines, data->contents[i]);
-                hadError = true;
-                (*num_tokens)--;
-            }
-        }
-    }
-
-    Token *token = &tokens[*num_tokens];
-    token->type = EOF_TOKEN;
-    token->lexeme = "";
-    (*num_tokens)++;
-    return tokens;
-}
-
-void print_token(Token token) {
-    printf("%s %s null\n", str_from_type(token.type), token.lexeme);
-}
-
-void print_tokens(Token *tokens, size_t num) {
-    for (int i = 0; i < num; i++) {
-        print_token(tokens[i]);
-    }
-}
-
 int main(int argc, char *argv[]) {
     // Disable output buffering
     setbuf(stdout, NULL);
@@ -172,25 +84,179 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
 
     if (strcmp(command, "tokenize") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
 
         FileData *file_data = read_file_contents(argv[2]);
-
-        size_t num_tokens = 0;
-        Token *tokens = scan_tokens(file_data, &num_tokens);
-        print_tokens(tokens, num_tokens);
-        if (hadError) return 65;
-
-        free(tokens);
+        if ( file_data->length > 0) {
+            interpreter(file_data);
+        } else {
+            printf("EOF  null\n");
+            return 0;
+        }
         free(file_data->contents);
         free(file_data);
+
     } else {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
     }
 
     return 0;
+}
+
+void interpreter(FileData *data) {
+    int position = 0;
+    data->line++;
+    bool error = false;
+    Token *token;
+    Token **validTokens = NULL;
+    int tokenCount = 0;
+
+    while(!isAtEnd(position, data->length)) {
+        if (data->contents[position] == '\n') {
+            data->line++;
+            position++;
+        } else {
+            token = scan_token(data, &position);
+
+            if (!(token->error)) {
+                validTokens = realloc(validTokens, sizeof(Token *) * (tokenCount + 1));
+                validTokens[tokenCount] = token;
+                tokenCount++;
+            } else {
+                fprintf(stderr, "[line %zu] Error: Unexpected character: %s\n", data->line, token->lexeme);
+                error = true;
+                free(token->lexeme);
+                free(token);
+            }
+        }
+    }
+    for (int i = 0; i < tokenCount; i++) {
+        print_token(*validTokens[i]);
+        free(validTokens[i]->lexeme);
+        free(validTokens[i]);
+    }
+    printf("EOF  null\n");
+    if (error) exit(65);
+    exit(0);
+}
+
+TokenList type_from_str(char *str) {
+    int n = sizeof(known_methods) / sizeof(known_methods[0]);
+    for (int i = 0; i < n; ++i) {
+        if (strcmp(str, known_methods[i].str) == 0) {
+            return known_methods[i].type;
+        }
+    }
+    return -1;
+}
+
+char *str_from_type(TokenList type) {
+    return known_methods[type].str;
+}
+
+Token *scan_token(FileData *data, int *position) {
+    Token *token = malloc(sizeof(Token));
+    token->lexeme = calloc(0, 3);
+    token->line = data->line;
+    token->error = 0;
+    char c = advance(data, position);
+    token->lexeme[0] = c;
+    switch (c) {
+        case '(': {
+            token->type = LEFT_PAREN;
+        } break;
+
+        case ')': {
+            token->type = RIGHT_PAREN;
+        } break;
+
+        case '{': {
+            token->type = LEFT_BRACE;
+        } break;
+
+        case '}': {
+            token->type = RIGHT_BRACE;
+        } break;
+
+        case ',': {
+            token->type = COMMA;
+        } break;
+
+        case '.': {
+            token->type = DOT;
+        } break;
+
+        case '-': {
+            token->type = MINUS;
+        } break;
+
+        case '+': {
+            token->type = PLUS;
+        } break;
+
+        case ';': {
+            token->type = SEMICOLON;
+        } break;
+
+        case '*': {
+            token->type = STAR;
+        } break;
+
+        case '!': {
+            token->type = match(data, position, '=') ? BANG_EQUAL : BANG;
+            if (token->type == BANG_EQUAL) {
+                token->lexeme[1] = '=';
+            }
+        } break;
+
+        case '=': {
+            token->type = match(data, position, '=') ? EQUAL_EQUAL : EQUAL;
+            if (token->type == EQUAL_EQUAL) {
+                token->lexeme[1] = '=';
+            }
+
+        } break;
+
+        case '<': {
+            token->type = match(data, position, '=') ? LESS_EQUAL : LESS;
+            if (token->type == LESS_EQUAL) {
+                token->lexeme[1] = '=';
+            }
+        } break;
+
+        case '>': {
+            token->type = match(data, position, '=') ? GREATER_EQUAL: GREATER;
+            if (token->type == GREATER_EQUAL) {
+                token->lexeme[1] = '=';
+            }
+        } break;
+
+        default: {
+            token->error = true;
+        }
+    }
+
+    return token;
+}
+
+bool match(FileData *data, int *position, char c) {
+    if (isAtEnd(*position, data->length) ||
+        data->contents[*position] != c) {
+        return false;
+    }
+    (*position)++;
+    return true;
+}
+
+char advance(FileData *data, int *position) {
+    char c = data->contents[(*position)++];
+    return c;
+}
+
+bool isAtEnd(int position, int len) { return position >= len; }
+
+void print_token(Token token) {
+    printf("%s %s null\n", str_from_type(token.type), token.lexeme);
 }
 
 FileData *read_file_contents(const char *filename) {
@@ -223,7 +289,7 @@ FileData *read_file_contents(const char *filename) {
     FileData *file_data = malloc(sizeof(FileData));
     file_data->contents = file_contents;
     file_data->length = file_size;
-    file_data->lines = 0;
+    file_data->line = 0;
     fclose(file);
 
     return file_data;
