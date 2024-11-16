2dd316f3ef0d0500a76b9500a6321c6c356dc16a
Julian Kylander <julian.kylander@gmail.com>
2024-11-16 02:21:56 +0100

    bugfix

--- src/main.c
+++ src/main.c
@@ -193,7 +193,7 @@ void print_token(Token token) {
     } else if (token.type == NUMBER) {
         printf("%s %s %.*f\n", str_from_token(token.type), token.lexeme, decimals_to_print(token.lexeme), *((double*)token.literal));
     } else if (token.type >= AND && token.type <= WHILE) {
-        char type[6];
+        char type[7];
         strcpy(type, token.lexeme);
         for (int i = 0, n = strlen(token.lexeme); i < n; i++) {
             type[i] = toupper(token.lexeme[i]);
