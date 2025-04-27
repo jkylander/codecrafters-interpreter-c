0dd2d677ed72783f53b8f44dd75278f18693f4e5
Julian Kylander <julian.kylander@gmail.com>
2025-04-27 23:42:25 +0200

    missing break; #2

--- src/compiler.c
+++ src/compiler.c
@@ -281,7 +281,7 @@ static void binary() {
         case TOKEN_BANG_EQUAL: emitBytes(OP_EQUAL, OP_NOT); break;
         case TOKEN_EQUAL_EQUAL: emitByte(OP_EQUAL); break;
         case TOKEN_GREATER: emitByte(OP_GREATER); break;
-        case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT);
+        case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT); break;
         case TOKEN_LESS: emitByte(OP_LESS); break;
         case TOKEN_LESS_EQUAL: emitBytes(OP_GREATER, OP_NOT); break;
         case TOKEN_PLUS: emitByte(OP_ADD); break;
