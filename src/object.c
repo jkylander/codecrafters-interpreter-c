30795b1411286bb8034d54ace323cc3f59067526
Julian Kylander <julian.kylander@gmail.com>
2025-05-01 01:17:16 +0200

    print fn

--- src/object.c
+++ src/object.c
@@ -55,7 +55,7 @@ static void printFunction(ObjFunction *function) {
         printf("<script>");
         return;
     }
-    printf("fn <%s>", function->name->chars);
+    printf("<fn %s>", function->name->chars);
 }
 
 void printObject(Value value) {
